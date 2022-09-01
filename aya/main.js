0e19a2be102431b02e28866d590bdb71127cbdc9
Michal Rostecki <vadorovsky@gmail.com>
2022-09-01 10:27:50 +0200

    aya: Moar updates
    Signed-off-by: Michal Rostecki <vadorovsky@gmail.com>

--- aya/main.js
+++ aya/main.js
@@ -2,9 +2,10 @@ import 'reveal.js/dist/reset.css';
 import 'reveal.js/dist/reveal.css';
 import 'reveal.js/dist/theme/moon.css';
 import Reveal from 'reveal.js';
+import Highlight from 'reveal.js/plugin/highlight/highlight.js';
 import Markdown from 'reveal.js/plugin/markdown/markdown.esm.js';
 
 let deck = new Reveal({
-  plugins: [Markdown]
+  plugins: [Highlight, Markdown],
 })
 deck.initialize();
